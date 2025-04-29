b8280086b5090915467db986f6e13f85df3d443f
Kwan <lightspots@hotmail.com>
2025-04-29 23:45:40 +0800

    feat(infra): optimize with arrays of cells to avoid ABA issues

--- infra/__infra_test.cpp
+++ infra/__infra_test.cpp
@@ -2,14 +2,14 @@
 #include <sstream>
 
 int main() {
-    RingBuffer<std::string> rb(5);
+    RingBuffer<std::string> rb(8);
     std::cout << rb << '\n';
     for (int i {0}; i < 3; ++i) rb.enqueue((std::stringstream() << 's' << i).str());
     std::cout << rb << '\n';
-    for (int i {40}; i < 45; ++i) rb.enqueue((std::stringstream() << 's' << i).str());
+    for (int i {20}; i < 45; ++i) rb.enqueue((std::stringstream() << 's' << i).str());
     std::cout << rb << '\n';
     std::string s;
-    while (!rb.empty()) std::cout << rb.dequeue(s) << ':' << s << ' ';
+    while (rb.dequeue(s)) std::cout << '[' << s << ']';
     std::cout << '\n';
     std::cout << rb << '\n';
     rb.enqueue("str");
--- infra/ringbuffer.h
+++ infra/ringbuffer.h
@@ -6,56 +6,90 @@
 #include <new>
 #include <iostream>
 #include <sstream>
+#include <cassert>
+
+template<typename T>
+struct Cell {
+    std::atomic_size_t ver_;
+    T data_;
+};
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const Cell<T>& cell) {
+    return os << '[' << cell.data_ << ':' << cell.ver_ << ']';
+}
 
 template<typename T>
 class RingBuffer {
     const static size_t defCap = 16;
     size_t cap_;
-    std::atomic_size_t beg_;
-    std::atomic_size_t end_;
-    T* data_;
+    size_t mask_;
+    std::atomic_size_t read_;
+    std::atomic_size_t write_;
+    Cell<T>* data_;
 public:
-    explicit RingBuffer(size_t capacity) : cap_{capacity <= 0 ? defCap : capacity}, beg_{0}, end_{0} {
-        data_ = static_cast<T*>(operator new(sizeof(T) * cap_, std::align_val_t(alignof(T))));
+    explicit RingBuffer(size_t capacity) : cap_{capacity <= 0 ? defCap : capacity}, mask_{cap_-1} {
+        assert(cap_ >= 2 && (cap_ & (cap_-1)) == 0); // cap must be power of 2
+        data_ = new Cell<T>[cap_];
+        for (size_t i {0}; i < cap_; ++i) data_[i].ver_ = i;
+        read_.store(0, std::memory_order_relaxed);
+        write_.store(0, std::memory_order_relaxed);
     }
     ~RingBuffer() noexcept {
-        for (size_t i {0}; i < cap_; ++i) {
-            data_[i].~T();
-        }
-        operator delete(data_);
+        delete[] data_;
     }
-    void enqueue(T x) {
-        size_t expEnd {end_.load()};
-        while (!end_.compare_exchange_strong(expEnd, expEnd + 1, std::memory_order_release));
-        new (&data_[wrap(expEnd)]) T{x};
-        size_t expBeg {beg_.load()};
-        // when full
-        while (end_ > cap_ 
-            && expBeg < end_ - cap_ 
-            && !beg_.compare_exchange_strong(expBeg, end_ - cap_ + 1, std::memory_order_release));
+    bool enqueue(const T& x) {
+        size_t write = write_.load(std::memory_order_relaxed);
+        Cell<T>* cell;
+        while (true) {
+            cell = &data_[wrap(write)];
+            size_t cellVer = cell->ver_.load(std::memory_order_acquire);
+            intptr_t diff = (intptr_t)cellVer - (intptr_t)write;
+            if (diff == 0) {
+                if (write_.compare_exchange_weak(write, write+1, std::memory_order_relaxed)) {
+                    break;
+                }
+            } else if (diff < 0) { // full
+                return false;
+            } else {
+                write = write_.load(std::memory_order_relaxed);
+            }
+        }
+        cell->data_ = x;
+        cell->ver_.store(write+1, std::memory_order_release);
+        return true;
     }
     bool dequeue(T& x) {
-        if (empty()) return false;
-        size_t expBeg {beg_.load()};
-        while (!beg_.compare_exchange_strong(expBeg, expBeg + 1, std::memory_order_acquire));
-        x = data_[wrap(expBeg)];
+        size_t read = read_.load(std::memory_order_relaxed);
+        Cell<T>* cell;
+        while (true) {
+            cell = &data_[wrap(read)];
+            size_t cellVer = cell->ver_.load(std::memory_order_acquire);
+            intptr_t diff = (intptr_t)cellVer - (intptr_t)(read+1); // b/c after write, version increments
+            if (diff == 0) {
+                if (read_.compare_exchange_weak(read, read+1, std::memory_order_relaxed)) break;
+            } else if (diff < 0) {
+                return false;
+            } else {
+                read = read_.load(std::memory_order_relaxed);
+            }
+        }
+        x = cell->data_;
+        cell->ver_.store(read + cap_, std::memory_order_release);
         return true;
     }
     std::stringstream ss() const {
         std::stringstream s;
-        s << "size:" << size() << ",beg:" << beg_ << ",end:" << end_ << ' ';
+        s << "size:" << size() << ",read:" << read_ << ",write:" << write_ << ' ';
         for (size_t i {0}; i < cap_; ++i) {
             s << '[' << data_[i] << ']';
         }
         return s;
     }
-
-    size_t size() const {return end_ - beg_;}
+    size_t size() const {return write_.load(std::memory_order_relaxed) - read_.load(std::memory_order_relaxed);}
     size_t cap() const {return cap_;}
-    bool empty() const {return size() == 0;}
 private:
     size_t wrap(size_t i) const {
-        return i % cap_;
+        return (i & mask_) ;
     }
 };
 
