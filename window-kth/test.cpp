cb7feabe5f7239dcab62f03e5b5ca5cfee92a6b7
Kwan <lightspots@hotmail.com>
2025-05-09 19:28:44 +0800

    feat(window-kth): optimize test case

--- window-kth/test.cpp
+++ window-kth/test.cpp
@@ -1,15 +1,20 @@
-#include "struct.hpp"
+#include "engine.hpp"
 #include <iostream>
 
 int main() {
     TradingEngine engine(5);
     engine.onOrder(TradingEngine::Order{0, 10});
-    engine.onOrder(TradingEngine::Order{0, 20});
-    engine.onOrder(TradingEngine::Order{0, 15});
-    engine.onOrder(TradingEngine::Order{0, 16});
+    engine.onOrder(TradingEngine::Order{1, 20});
+    engine.onOrder(TradingEngine::Order{3, 15});
+    engine.onOrder(TradingEngine::Order{6, 16});
 
-    auto [has, price] = engine.onNotify(0, 3);
+    std::cout << std::boolalpha;
+    auto [has, price] = engine.onNotify(6, 3);
     std::cout << has << ',' << price << '\n';
+    auto [has1, price1] = engine.onNotify(7, 3);
+    std::cout << has1 << ',' << price1 << '\n';
+    auto [has2, price2] = engine.onNotify(11, 1);
+    std::cout << has2 << ',' << price2 << '\n';
 
     return 0;
 }
